af7e35148cce4cebb38400002274f3e4d808764c
Natnael Biazen <natnaelpom0@gmail.com>
2024-12-26 20:08:16 +0300

    Button Fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ const Home = () => {
       </p>
       <button
         onClick={handleStart}
-        className="px-8 py-2 bg-primary text-text rounded-xl font-bold text-xl font-retro border-4 border-accent hover:bg-accent hover:text-black transition duration-300 transform hover:scale-105"
+        className="px-8 py-2 bg-primary text-text rounded-xl font-bold text-xl font-retro border-2 border-accent hover:bg-accent hover:text-primary transition duration-300 transform hover:scale-105"
       >
         START GAME
       </button>
